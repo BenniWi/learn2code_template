a076f636f5912b067a278af47bb04433379b27b7
LeonieOwO <122836295+LeonieOwO@users.noreply.github.com>
2023-04-19 07:54:00 +0000

    first lesson

---
+++ build/CMakeFiles/basics_in_Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basics_in_Cpp.dir/C++/Unterricht_1.cpp.o"
+  "CMakeFiles/basics_in_Cpp.dir/C++/Unterricht_1.cpp.o.d"
+  "basics_in_Cpp"
+  "basics_in_Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basics_in_Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/basics_in_Cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for basics_in_Cpp.
+# This may be replaced when dependencies are built.
